5819d6cd7f524cb5650376833740388adf2b3063
dudikpal <59420449+dudikpal@users.noreply.github.com>
2021-07-01 22:50:03 +0200

    #1: video tasks - Employees springboot - státuszkódok és hibakezelés task done

--- video-tasks/employees/src/main/java/employees/EmployeesController.java
+++ video-tasks/employees/src/main/java/employees/EmployeesController.java
@@ -1,9 +1,13 @@
 package employees;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.zalando.problem.Problem;
+import org.zalando.problem.Status;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,13 +27,19 @@ public class EmployeesController {
     }
 
     @GetMapping("/{id}")
+    public EmployeeDto findEmployeeById(@PathVariable("id") long id) {
+
+        return employeesService.findEmployeeById(id);
+    }
+
+    /*@GetMapping("/{id}")
     public ResponseEntity findEmployeeById(@PathVariable("id") long id) {
         try {
             return ResponseEntity.ok(employeesService.findEmployeeById(id));
         } catch (Exception e) {
             return ResponseEntity.notFound().build();
         }
-    }
+    }*/
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -46,6 +56,21 @@ public class EmployeesController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteEmployee(@PathVariable("id") long id) {
         employeesService.deleteEmployee(id);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<Problem> handleNotFound(IllegalArgumentException iae) {
+        Problem problem = Problem.builder()
+                .withType(URI.create("employees/not-found"))
+                .withTitle("Not found")
+                .withStatus(Status.NOT_FOUND)
+                .withDetail(iae.getMessage())
+                .build();
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .contentType(MediaType.APPLICATION_PROBLEM_JSON)
+                .body(problem);
     }
 
 }
