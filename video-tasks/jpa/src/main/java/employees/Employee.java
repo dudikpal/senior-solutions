a8c8c03842d429936244e410fc6e4fe66a6fb01c
dudikpal <59420449+dudikpal@users.noreply.github.com>
2021-07-21 23:32:56 +0200

    #1: JPA video task - További egyszerű műveletek JPA-val done

--- video-tasks/jpa/src/main/java/employees/Employee.java
+++ video-tasks/jpa/src/main/java/employees/Employee.java
@@ -35,4 +35,12 @@ public class Employee {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
--- video-tasks/jpa/src/main/java/employees/EmployeesMain.java
+++ video-tasks/jpa/src/main/java/employees/EmployeesMain.java
@@ -3,6 +3,7 @@ package employees;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class EmployeesMain {
 
@@ -11,9 +12,33 @@ public class EmployeesMain {
         EntityManagerFactory factory = Persistence.createEntityManagerFactory("pu");
         EntityManager em = factory.createEntityManager();
         em.getTransaction().begin();
-        em.persist(new Employee("John Doe"));
-        em.persist(new Employee("Jack Doe"));
+        Employee employee = new Employee("John Doe");
+        Employee employee2 = new Employee("Jack Doe");
+        em.persist(employee);
+        em.persist(employee2);
         em.getTransaction().commit();
+        long id = employee2.getId();
+        em = factory.createEntityManager();
+        employee = em.find(Employee.class, id);
+        System.out.println(employee);
+
+        em.getTransaction().begin();
+        employee = em.find(Employee.class, id);
+        employee.setName("JJJ Doe");
+        em.getTransaction().commit();
+
+        List<Employee> employees = em.createQuery("select e from Employee e", Employee.class)
+                .getResultList();
+        System.out.println(employees);
+
+        em.getTransaction().begin();
+        employee = em.find(Employee.class, id);
+        em.remove(employee);
+        em.getTransaction().commit();
+
+        employees = em.createQuery("select e from Employee e", Employee.class).getResultList();
+        System.out.println(employees);
+
         em.close();
         factory.close();
     }
