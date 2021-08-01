7e4066e76489a99f9a9fbf472d42521bab59042e
dudikpal <59420449+dudikpal@users.noreply.github.com>
2021-08-02 00:07:59 +0200

    #1: JPA task - Entitások konfigurálása done

--- activitytracker-jpa/src/main/java/activitytracker/Activity.java
+++ activitytracker-jpa/src/main/java/activitytracker/Activity.java
@@ -16,13 +16,14 @@ public class Activity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "start_time")
+    @Column(name = "start_time", nullable = false)
     private LocalDateTime startTime;
 
-    @Column(name = "description")
+    @Column(name = "description", nullable = false, length = 200)
     private String description;
 
-    @Column(name = "type")
+    @Column(name = "type", nullable = false, length = 20)
+    @Enumerated(EnumType.STRING)
     private ActivityType type;
 
     public Activity(LocalDateTime startTime, String description, ActivityType type) {
